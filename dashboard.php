b48eb3fca64acf26e043db167cbc6ae4372d66fc
marcrine-geek <marcrinemm@gmail.com>
2022-04-26 15:53:43 +0300

    dashboard changes

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,10 @@
 <?php
-
+require 'connection.php';
+//fetching user name
+if (isset($_POST['uname']))
+{
+//    echo $_POST['uname'];
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -9,10 +14,32 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 <body>
+<div class="container">
+    <div class="row">
+        <h1>Welcome <?php echo $_POST['uname']; ?></h1>
+    </div>
+</div>
 
-<h1>hello</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <h1>Column 1 Side bar</h1>
+        </div>
+        <div class="col-md-4">
+            <h1>Column 2 Inner content</h1>
+        </div>
+        <div class="col-md-4">
+            <h1>More info</h1>
+        </div>
+    </div>
+</div>
+
 
 </body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </html>
